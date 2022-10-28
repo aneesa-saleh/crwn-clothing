027c501ed40654b54bff47a0cc264902d6a7881e
Aneesa Saleh <aneesa.saleh@gmail.com>
2022-10-28 17:04:59 +0100

    Add category components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
+import Categories from "./components/categories/categories.component";
+
 function App() {
   const categories = [
-    { id: 1, title: 'Hats' },
-    { id: 2, title: 'Jackets' },
-    { id: 3, title: 'Sneakers' },
-    { id: 4, title: 'Womens' },
-    { id: 5, title: 'Mens' },
+    {
+      "id": 1,
+      "title": "Hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      "id": 2,
+      "title": "Jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      "id": 3,
+      "title": "Sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "Womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      "id": 5,
+      "title": "Mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
   ]
   return (
-    <div className="categories-container">
-      {categories.map(({ title }) => (<div className="category-container">
-        {/*<img />*/}
-        <div className="category-body-container">
-          <h2>{title}</h2>
-          <p>Shop now</p>
-        </div>
-      </div>))}
-
-    </div>
+    <Categories categories={categories} />
   );
 }
 
