f10799e76d9298b8690795ea8faaabb207d3bbd6
Aneesa Saleh <aneesa.saleh@gmail.com>
2023-01-03 12:52:33 +0100

    Connect sign up form to firebase

--- src/components/sign-up/sign-up-form.component.jsx
+++ src/components/sign-up/sign-up-form.component.jsx
@@ -1,4 +1,7 @@
+import { async } from '@firebase/util';
 import { useState } from 'react';
+
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
 const defaultFormFields = {
     displayName: '',
@@ -11,6 +14,33 @@ function SignUpForm() {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
 
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (password !== confirmPassword) {
+            alert('Passwords do not match');
+            return;
+        }
+
+        try {
+            const { user } = await createAuthUserWithEmailAndPassword(email, password);
+
+            await createUserDocumentFromAuth(user, { displayName });
+            resetFormFields();
+        } catch(error) {
+            if (error.code === 'auth/email-already-in-use') {
+                alert('User email already in use')
+            } else {
+                console.log('User creation encountered an error');
+                console.log(error);
+            }
+        }
+    }
+
     const handleChange = (event) => {
         const { name, value } = event.target;
         setFormFields((prevFormFields) => ({ ...prevFormFields, [name]: value }));
@@ -19,7 +49,7 @@ function SignUpForm() {
     return (
         <div>
             <h1>Sign up with your email and password</h1>
-            <form onSubmit={() => {}}>
+            <form onSubmit={handleSubmit}>
                 <label>Display Name</label>
                 <input type={'text'} required onChange={handleChange} name="displayName" value={displayName} />
 
